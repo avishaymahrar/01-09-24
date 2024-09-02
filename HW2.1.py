5ed965a0ad92a86f49a5d196a31990e56d516d16
avishaymahrar <avishay.mahrar@gmail.com>
2024-09-02 11:56:40 +0300

    Add files via upload

---
+++ HW2.1.py
@@ -0,0 +1,8 @@
+##start
+x: int = int ( input (" what is x "));
+y: int = int ( input (" what is y "));
+z: int = int ( input (" what is z "));
+print ('x + y + z =', x + y + z );
+print ('x * y * z =', x * y * z );
+##end
+##test
